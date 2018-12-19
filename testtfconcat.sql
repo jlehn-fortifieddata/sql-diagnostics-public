42713ffff54f72282f35c46a5fca5886a1356734
jlehn-fortifieddata <41924870+jlehn-fortifieddata@users.noreply.github.com>
2018-12-19 12:02:17 -0500

    Update testtfconcat.sql

--- testtfconcat.sql
+++ testtfconcat.sql
@@ -1,19 +1,15 @@
-/*
-SELECT TOP (1)
-	*
-FROM
-	[FDDBA].[dbo].[config_fddba2_history] X
-WHERE
-	X.[name] = 'trace_flags_existing'
-ORDER BY
-	X.CreatedOn DESC;
+SET NOCOUNT ON;
 
-SELECT
-	*
-FROM
-	[FDDBA].[dbo].[config_fddba2];
-*/
+USE [FDDBA];
 
+DECLARE @DefinedStartupTraceFlags nvarchar(3950);
+DECLARE @RevisedStartupTraceFlags nvarchar(3950);
+DECLARE @DebugOutput nvarchar(4000);
+
+-- Set this value to 1 if you actually want the config values to be updated.
+DECLARE @PerformUpdate bit = 1;
+
+-- Create a temporary table to hold active trace flags on the instance.
 CREATE TABLE
 	#ActiveTraceFlags
 	(
@@ -23,18 +19,18 @@ CREATE TABLE
 		,[Session] int
 	);
 
+-- Insert active trace flags into the temporary table.
 INSERT INTO
 	#ActiveTraceFlags
 EXEC ('DBCC TRACESTATUS (-1) WITH NO_INFOMSGS');
 
-SELECT
-	*
-FROM
-	#ActiveTraceFlags;
+-- Remove any trace flags that are not global.
+DELETE
+	#ActiveTraceFlags
+WHERE
+	[Global] != 1;
 
-DECLARE @DefinedStartupTraceFlags nvarchar(3950);
-DECLARE @RevisedStartupTraceFlags nvarchar(3950);
-
+-- Get the current FDDBA trace flag config value.
 SELECT TOP (1)
 	@DefinedStartupTraceFlags = [value]
 FROM
@@ -44,16 +40,14 @@ WHERE
 ORDER BY
 	[CreatedOn] DESC;
 
-SELECT
-	@DefinedStartupTraceFlags;
-
+-- Split the trace flags from the FDDBA config into separate rows in the temp table.
 ;WITH FDDBATraceFlags AS
 (
 	SELECT
 		LEFT(X.[val], CHARINDEX(N'=', X.[val], 1) - 1) AS TraceFlag
 		,SUBSTRING(X.[val], CHARINDEX(N'=', X.[val], 1) + 1, 1) AS TraceFlagValue
 	FROM
-		dbo.fSplitNString(@DefinedStartupTraceFlags, N',') X
+		[FDDBA].dbo.fSplitNString(@DefinedStartupTraceFlags, N',') X
 	WHERE
 		LEN(LTRIM(RTRIM(X.[val]))) > 0
 )
@@ -81,23 +75,58 @@ WHERE
 			#ActiveTraceFlags Y
 	);
 
-SELECT
-	*
-FROM
-	#ActiveTraceFlags;
-
+-- Gather up all of the trace flags from the temp table and put them into a new config value.
 SELECT
 	@RevisedStartupTraceFlags = STUFF((
 		SELECT
 			N',' + CAST(X.[TraceFlag] AS nvarchar(10)) + N'=' + CAST(X.[Status] AS nvarchar(10))
 		FROM
 			#ActiveTraceFlags X
+		ORDER BY
+			X.TraceFlag
 		FOR XML PATH('')), 1, 1, '') + N',';
 
-SELECT
-	@DefinedStartupTraceFlags;
+SET @DebugOutput = N'Server name:  ' + @@SERVERNAME + CHAR(13)
+	+ N'Current FDDBA trace flags:  ' + @DefinedStartupTraceFlags + CHAR(13)
+	+ N'New FDDBA trace flags:  ' + @RevisedStartupTraceFlags
 
-SELECT
-	@RevisedStartupTraceFlags;
+PRINT @DebugOutput;
+
+-- If we are going to update the config table, only do so if the trace flags are not defined properly.
+IF (@PerformUpdate = 1 AND @RevisedStartupTraceFlags != @DefinedStartupTraceFlags)
+BEGIN
+	-- Write out the current value to the history table, just in case.
+	INSERT INTO
+		[FDDBA].[dbo].[config_fddba2_history]
+		(
+			[name]
+			,[domain]
+			,[CreatedOn]
+			,[UpdatedOn]
+			,[value]
+			,[valuebig]
+			,[comment]
+		)
+	SELECT
+		[name]
+		,[domain]
+		,[CreatedOn]
+		,[UpdatedOn]
+		,[value]
+		,[valuebig]
+		,[comment]
+	FROM
+		[FDDBA].[dbo].[config_fddba2]
+	WHERE
+		[name] = 'trace_flags';
+
+	UPDATE
+		[FDDBA].[dbo].[config_fddba2]
+	SET
+		UpdatedOn = GETDATE()
+		,[value] = @RevisedStartupTraceFlags
+	WHERE
+		[name] = 'trace_flags';
+END
 
 DROP TABLE #ActiveTraceFlags;
